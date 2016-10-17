a1e36709e22b61220de5384c6e7003dfd09175d0
David Heim <davidheim@bluewin.ch>
2016-10-17 17:57:13 +0200

    updated project to webpack-dev-server, use hash history instead of browser history

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,28 +1,30 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 
-import Header from 'components/Header'; 
+import Header from 'components/Header';
 
 import PatientList from 'components/PatientList';
 import PatientForm from 'components/PatientForm';
+import PrescriptionForm from 'components/PrescriptionForm';
 
 import 'font-awesome-webpack';
 import 'less/imports';
 
 class LandingPage extends Component {
-	render() {
-		return (<div id="main-container">
-			<Header/>
-			<div id="main-content">
-				{this.props.children || <PatientList/>}
-			</div>
-		</div>);
-	}
+    render() {
+        return (<div id="main-container">
+            <Header/>
+            <div id="main-content">
+                {this.props.children || <PatientList/>}
+            </div>
+        </div>);
+    }
 }
 
-render(<Router history={browserHistory}>
-	<Route path="/" component={LandingPage}>
-		<Route path="/patient/:id" component={PatientForm} />
-	</Route>
+render(<Router history={hashHistory}>
+    <Route path="/" component={LandingPage}>
+        <Route path="/patient/:id" component={PatientForm}/>
+        <Route path="/prescription" component={PrescriptionForm}/>
+    </Route>
 </Router>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,60 @@
-const join							= require('path').join;
-const webpack						= require('webpack');
+const join = require('path').join;
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const src								= join(__dirname, 'client/src');
-const target						= join(__dirname, 'client/public');
+const src = join(__dirname, 'client/src');
+const target = join(__dirname, 'client/public');
 
 module.exports = {
-	devtol: 'eval',
-	entry: `${src}/index.jsx`,
-	output: {
-		path: target,
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx|\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015', 'stage-3', 'react']
-				}
-			},
-			{
-				test: /\.less$/,
-				loader: ExtractTextPlugin.extract('css?sourceMap!less?sourceMap')
-			},
-			{
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: "url-loader?limit=10000&name=/[name].[ext]&mimetype=application/font-woff"
-			},
-			{
-				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: "file-loader?name=/[name].[ext]"
-			}
-		]
-	},
-	resolve: {
-		extensions: ['', '.jsx', '.js', '.json', '.less'],
-		alias: {
-			components: join(src, 'components'),
-			services: join(src, 'services'),
-			less: join(src, 'less')
-		}
-	},
-	plugins: [
-		new ExtractTextPlugin('bundle.css'),
-		new webpack.ProvidePlugin({
-			'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-		})
-	]
+    devtol: 'eval',
+    entry: `${src}/index.jsx`,
+    output: {
+        path: target,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx|\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'stage-3', 'react']
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('css?sourceMap!less?sourceMap')
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&name=/[name].[ext]&mimetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader?name=/[name].[ext]"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.jsx', '.js', '.json', '.less'],
+        alias: {
+            components: join(src, 'components'),
+            services: join(src, 'services'),
+            less: join(src, 'less')
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css'),
+        new webpack.ProvidePlugin({
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
+    ],
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                secure: false
+            }
+        }
+    }
 };
