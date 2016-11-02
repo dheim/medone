1fab0ce42565ce241e69445a303452aa15629e15
Thilo Holzer <th@holzer.com>
2016-11-02 18:14:25 +0100

    added material-ui

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,25 +1,33 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import Header from 'components/Header';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import PatientList from 'components/PatientList';
 import PatientForm from 'components/PatientForm';
 import PrescriptionForm from 'components/PrescriptionForm';
 import PrescriptionList from 'components/PrescriptionList';
 
+import AppBar from 'material-ui/AppBar';
+
 import 'font-awesome-webpack';
 import 'less/imports';
 
+
 class AppComponent extends Component {
     render() {
-        return (<div id="main-container">
-            <Header/>
-            <div id="main-content">
-                {this.props.children}
+        return (<MuiThemeProvider>
+            <div>
+                <AppBar title="medONE"
+                            showMenuIconButton={true}
+                            iconClassNameLeft="fa fa-medkit"
+                            />
+                    <div id="main-view">{this.props.children}</div>
             </div>
-        </div>);
+        </MuiThemeProvider>);
     }
 }
 
