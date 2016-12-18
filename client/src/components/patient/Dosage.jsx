285f14df8357a43df4f1f5d38232b18af4b37576
Thilo Holzer <th@holzer.com>
2016-12-18 17:43:06 +0100

    fixed styling of dosage-field

--- client/src/components/patient/Dosage.jsx
+++ client/src/components/patient/Dosage.jsx
@@ -8,6 +8,7 @@ class Dosage extends Component {
         return (
             <div>
                 <TextField type="number" name={this.props.name} min="0" max="1000" floatingLabelText={this.props.label}
+                					 style={{position: 'relative', top: '-12px'}}
                            onChange={(event, value) => this.props.onChange(event, value)}/>
                 <span>{this.props.unity}</span>
             </div>
